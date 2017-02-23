a4716480922dc79212f08742e57aa72e3fc3e81b
sergio <sergio@MSI>
2017-02-23 17:17:14 +0100

    Clase carta terminada

--- src/Models/Carta.java
+++ src/Models/Carta.java
@@ -7,10 +7,10 @@ public class Carta {
 	private String[] nombreCarta;
 
 	public Carta(int numero, int palo) {
-		String[] cartas={"as","dos","tres","cuatro","cinco","seis","siete","Sota","Caballo","Rey"};
-		this.nombreCarta=cartas;
-		String[] palos={"Bastos","Copas","Espadas","Oros"};
-		this.nombrePalo=palos;
+		String[] cartas = { "as", "dos", "tres", "cuatro", "cinco", "seis", "siete", "Sota", "Caballo", "Rey" };
+		this.nombreCarta = cartas;
+		String[] palos = { "Bastos", "Copas", "Espadas", "Oros" };
+		this.nombrePalo = palos;
 		this.numero = numero-1;
 		this.palo = palo-1;
 	}
@@ -23,7 +23,7 @@ public class Carta {
 	 * @param identificador
 	 */
 	public Carta(int identificador) {
-		new Carta((identificador % 10), (identificador / 10));
+		this ((identificador % 10), (identificador / 10));
 	}
 
 	/**
@@ -33,29 +33,74 @@ public class Carta {
 	 *            de carta
 	 * @return Nos devuelve el ID correspondiente a una carta
 	 */
-	public int getId(int numero, int palo) {
+	public int getId() {
 		String aux = "";
-		aux += palo;
-		aux += numero;
+		aux += this.palo;
+		aux += this.numero;
 		return Integer.parseInt(aux);
 	}
 
 	public int getNumero() {
-		return numero;
+		return this.numero +1;
 	}
 
 	public int getPalo() {
-		return palo;
+		return this.palo+1;
 	}
 
-	public String getNombrePalo(int palo) {
-		return nombrePalo[palo];
+	public String getNombrePalo() {
+		return nombrePalo[this.palo];
 	}
 
-	public String getNombreCarta(int carta){
-		return nombreCarta[palo];
+	public String getNombreCarta() {
+		return nombreCarta[this.numero];
 	}
-	/*public int getValorTute(int numeroCarta){
-		
-	}*/
+
+	/**
+	 * Muestra el valor en el juego Tute Si como valor muestra un numero
+	 * negativo, has introducido un numero de carta incorrecto.
+	 * 
+	 * @param numeroCarta
+	 *            es el numero de nuestra carta dentro de su palo
+	 * @return el valor dentro del Tute
+	 */
+	public int getValorTute() {
+		if (this.numero == 1) {
+			return 11;
+		} else if (this.numero == 3) {
+			return 10;
+		} else if (this.numero == 8) {
+			return 2;
+		} else if (this.numero == 9) {
+			return 9;
+		} else if (this.numero == 10) {
+			return 4;
+		} else {
+			return -1;
+		}
+	}
+
+	public int getValorMus() {
+		if (this.numero == 1 || this.numero == 2) {
+			return 1;
+		} else if (this.numero == 3 || (this.numero >= 8 && this.numero <= 10)) {
+			return 10;
+		} else {
+			return this.numero;
+		}
+	}
+
+	public float getValor7yMedia() {
+		if (this.numero >= 8 && this.numero <= 10) {
+			return 0.5f;
+		} else {
+			return (float) this.numero;
+		}
+	}
+
+	@Override
+	public String toString() {
+		//return "+++";
+		return getNombreCarta()+" de "+getNombrePalo();
+	}
 }
